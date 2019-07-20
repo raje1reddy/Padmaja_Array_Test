a06b5a0bbe0ae66252a9332ac82996c642f0cfe6
unknown <raje1reddy@gmail.com>
2019-07-21 01:52:26 +0530

    code access and prints all the array elements list

--- src/main/java/PadmajaArrayTest.java
+++ src/main/java/PadmajaArrayTest.java
@@ -1,27 +1,18 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 import java.util.Scanner;
-
-
-
-public class PadmajaStringReverseTest1 {
+import java.util.ArrayList;
+//code accesses and prints all the elements of array list
+public class PadmajaArrayTest {
     public static void main(String[] args){
-        System.out.println("Enter string to reverse:");
-
-        Scanner read = new Scanner(System.in);
-        String str = read.nextLine();
-
-        StringBuilder sb = new StringBuilder();
-
-        for(int i = str.length() - 1; i >= 0; i--)
-        {
-            sb.append(str.charAt(i));
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("one");
+        list.add("two");
+        list.add("four");
+        list.add(2, "three");
+        for(String element : list){
+            System.out.println(element);
         }
-        System.out.println("Reversed string is:");
-        System.out.println(sb.toString());
-    }
 
+    }
 }
+
